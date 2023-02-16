dd2ba9448054ce8b5a5e38606ddd94a3e582d2ec
Van Chien <phuthienchien@gmail.com>
2023-02-17 01:31:49 +0700

    update login submodule

---
+++ backend/storage/framework/views/92ffc5c0be315c4863364c143ef97db367a1b1ea.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+   <form action="api/register" method="POST">
+    <?php echo csrf_field(); ?>
+    <input type="text" value= 'wefwe  fwef   fwe  f '>
+    <input type="password" value="111111">
+    <input type="email" name="" id="" value="123@gmai.com">
+    <button type="submit">gui</button>
+   </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html><?php /**PATH C:\xampp\htdocs\QuanLyChiTieuR-L\backend\resources\views/test.blade.php ENDPATH**/ ?>
